f93cb931d973be06abdf3626a0ad369b880ff2fb
CaioTrindade <caio.trindade@idus.com.br>
2026-03-24 09:32:21 -0300

    Desacopla o mapeamento do service e transference para o Dominio

--- operations-service/src/main/java/com/sensor_data_ingestion/operations_service/domain/model/LeituraDoSensor.java
+++ operations-service/src/main/java/com/sensor_data_ingestion/operations_service/domain/model/LeituraDoSensor.java
@@ -1,5 +1,7 @@
 package com.sensor_data_ingestion.operations_service.domain.model;
 
+import com.sensor_data_ingestion.operations_service.domain.either.Either;
+import com.sensor_data_ingestion.operations_service.domain.error.ErroDeIngestao;
 import com.sensor_data_ingestion.operations_service.domain.valueObjects.SensorId;
 import com.sensor_data_ingestion.operations_service.domain.valueObjects.Unidade;
 import com.sensor_data_ingestion.operations_service.domain.valueObjects.ValorMedicao;
@@ -11,4 +13,17 @@ public record LeituraDoSensor(
         ValorMedicao valor,
         Unidade unidade,
         Instant timestamp
-) {}
+) {
+    public static Either<ErroDeIngestao, LeituraDoSensor> criar(
+            String rawSensorId, double rawValor, String rawUnidade, long rawTimeStamp) {
+
+        return SensorId.of(rawSensorId)
+                .flatMap(sensorId -> ValorMedicao.of(rawValor)
+                        .flatMap(valor -> Unidade.of(rawUnidade)
+                                .map(unidade ->
+                                        new LeituraDoSensor(sensorId, valor, unidade, Instant.ofEpochMilli(rawTimeStamp))
+                                )));
+    }
+
+
+}
--- operations-service/src/main/java/com/sensor_data_ingestion/operations_service/repository/entity/LeituraDoSensorEntity.java
+++ operations-service/src/main/java/com/sensor_data_ingestion/operations_service/repository/entity/LeituraDoSensorEntity.java
@@ -1,5 +1,29 @@
 package com.sensor_data_ingestion.operations_service.repository.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "leituras_sensor")
 public class LeituraDoSensorEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sensor_id")
+    private String sensorId;
+
+    private double valor;
+
+    private String unidade;
+
+    @Column(name = "instante_processamento")
+    private Instant timestamp;
 }
